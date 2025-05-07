733b4c5258bfbc087ca57324c6e4fde16cefd335
ebyvick <cjgeneral12@gmail.com>
2025-05-07 03:40:25 -0700

    my first init

---
+++ classwork.py
@@ -0,0 +1,31 @@
+# i# Collect user input
+# Number = int(input("Enter Number:"))
+
+# # Check if the number is Even or not
+# if (Number % 2 == 0):
+#     print(f"{Number} is an Even.")
+# # else:
+# #     print(f"{Number
+             
+
+# # ada is a girlS
+# print
+
+
+# num=[1,2,3,4,5]
+# num_squared=[]
+# for i in num:
+# i**=2
+# num_squared.append(i)
+
+# print(num_squared)
+# n=7
+# for j in range(n,o,-i):
+#     for j in range(n-i)
+
+
+# n-7
+# for i in range(n,o-i):
+#     for j in range(n-i):
+# print('--k',end'')
+for j in range(ada is a girl)
